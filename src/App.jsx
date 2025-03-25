b183d6e851048c439615252968c0f0773de1bf94
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-25 23:41:01 +0530

    update routing in App component to set default path to HomePage and ensure consistent navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import HomePage from './pages/HomePage'
-import SignUpPage from './pages/SignUpPage'
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import DashboardPage from './pages/DashboardPage'
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 function App() {
-
-
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path='/dashboard' element={<DashboardPage/>}/>
-      </Routes>
-    </div>
-  </Router>
-  )
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
