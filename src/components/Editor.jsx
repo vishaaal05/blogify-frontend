a7a5ad96d58641ad76aaf6b22048975001e2085b
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-31 23:01:43 +0530

    refactor: improve formatting and consistency in Editor component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import { Editor } from "primereact/editor";
 
 export default function BasicDemo() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    return (
-        <div className="card">
-            <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
-        </div>
-    )
+  return (
+    <div className="card">
+      <Editor
+        value={text}
+        onTextChange={(e) => setText(e.htmlValue)}
+        style={{ height: "320px" }}
+      />
+    </div>
+  );
 }
