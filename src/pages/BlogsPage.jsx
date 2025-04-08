1e6c032a9367a9d2295b53d63bd2900effaafbd1
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-09 00:03:22 +0530

    refactor: improve layout structure in BlogsPage component for better readability

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -3,13 +3,14 @@ import { Header } from "../components/Header";
 import BlogCards from "../components/BlogCards";
 
 const BlogsPage = () => {
-  return <div>
-    <Header/>
-    <div className="py-10">
-    <BlogCards/>
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <div className="container mx-auto py-10">
+        <BlogCards />
+      </div>
     </div>
-  
-  </div>;
+  );
 };
 
 export default BlogsPage;
